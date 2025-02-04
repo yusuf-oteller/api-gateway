89dbb7d1efb9d53a90145b760a05768f950ba5cb
yusufbulac <ybulac@gmail.com>
2025-02-05 00:08:18 +0300

    Initial commit: Basic project structure and configurations

---
+++ src/test/java/com/hotelapp/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelapp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
